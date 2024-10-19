4cf0efc866addc7c8a65377f23c791194cf138d4
Ethan Crann <ecrann@protonmail.com>
2024-10-19 19:21:45 -0230

    Setting to....(0.001,0.001,0.001)...fixes...the bug?

--- src/ThreeViewer.tsx
+++ src/ThreeViewer.tsx
@@ -1,13 +1,17 @@
 import { Environment, useGLTF, CameraControls} from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useEffect, useRef, useState } from "react"
-import { Group, Object3D, Sphere, Vector3 } from "three"
+import { Group, Object3D, SkinnedMesh, Sphere, Vector3 } from "three"
 import { useMemo } from "react"
+import { useHelper } from "@react-three/drei"
+import { BoxHelper } from "three"
 
 import { Box3 } from "three"
 type GlbModelProps = {
     name: string
 }
+
+
 
 const GlbModel = ({name}: GlbModelProps) => {
     const camControlsRef = useRef<CameraControls>(null);
@@ -18,15 +22,25 @@ const GlbModel = ({name}: GlbModelProps) => {
         // setMeshVisible(false);
     }
     const gltf = useGLTF(name);    
-    gltf.scene.updateMatrixWorld(true);
+    const skinnedMeshes: Array<SkinnedMesh> = [];
+    gltf.scene.traverse((child) => {
+        if (child.type === "SkinnedMesh") {
+            skinnedMeshes.push(child as SkinnedMesh);
+        }
+    });
+    
+    // gltf.scene.updateMatrixWorld(true);
+    // gltf.scene.matrixWorldAutoUpdate = true
     const box = new Box3().setFromObject(gltf.scene);
     const bsphere = useMemo(() => {return new Sphere()},[]);
     box.getBoundingSphere(bsphere);
     const groupRef = useRef<Group>(new Group());
     const meshRef = useRef<Object3D>(new Object3D());
+    const box3 = new Box3().setFromObject(gltf.scene);
+    // useHelper(meshRef, BoxHelper, 'cyan');
 
-    const currentScale = new Vector3(0,0,0);
-    const introAnimation = true;
+    const currentScale = new Vector3(0.001,0.001,0.001);
+    const [introAnimation,setIntroAnimation] = useState<boolean>(true);
 
 
     useEffect(() => {
@@ -36,11 +50,29 @@ const GlbModel = ({name}: GlbModelProps) => {
     }, [box, bsphere]);
 
     useFrame(() => {
-        // if (introAnimation) {
-        //     groupRef.current.scale.copy(currentScale);
-        //     currentScale.lerp(new Vector3(1,1,1), 0.1);            
-        // }
-        // setMeshVisible(true);
+        if (skinnedMeshes.length > 0) {
+            skinnedMeshes.forEach((mesh: SkinnedMesh) => {
+                mesh.geometry.computeBoundingBox();
+                mesh.computeBoundingBox();
+                mesh.updateMatrixWorld(true);
+                mesh.matrixWorldAutoUpdate = true;
+                mesh.matrixWorldNeedsUpdate = true;
+                mesh.matrixAutoUpdate = true;
+            })
+        }
+        if (introAnimation) {
+            groupRef.current.scale.copy(currentScale);
+            groupRef.current.updateMatrixWorld(true);
+            groupRef.current.matrixWorldAutoUpdate = true;
+            groupRef.current.matrixWorldNeedsUpdate = true;
+            groupRef.current.matrixAutoUpdate = true;
+
+            meshRef.current.updateMatrixWorld(true);
+            meshRef.current.matrixWorldAutoUpdate = true;
+            meshRef.current.matrixWorldNeedsUpdate = true;
+            meshRef.current.matrixAutoUpdate = true;
+            currentScale.lerp(new Vector3(1,1,1), 0.1);
+        }
     });
 
     return (
@@ -52,13 +84,13 @@ const GlbModel = ({name}: GlbModelProps) => {
 }
 
 const GalleryController = () => {
-    const [selectedModel, setSelectedModel] = useState<string>("Gump.glb");
+    const [selectedModel, setSelectedModel] = useState<string>("MansonFish.glb");
 
     
     const cycleSelectedModel = () => {
         console.log("Cycling model");
         if (selectedModel === "Gump.glb") {
-            setSelectedModel("MansonFish.glb")
+            setSelectedModel("egg.glb")
         } else {
             setSelectedModel("Gump.glb")
         }
