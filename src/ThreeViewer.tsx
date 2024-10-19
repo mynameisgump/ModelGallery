7a0d609b2185df3094eee23153f7d15e3bfadf1b
Ethan Crann <ecrann@protonmail.com>
2024-10-19 16:26:45 -0230

    Currently still trying to figure out a way to make a clean animation regardless of scale. Don't think I'll be able to make this work tho

--- src/ThreeViewer.tsx
+++ src/ThreeViewer.tsx
@@ -1,44 +1,52 @@
-import { OrbitControls, Gltf, Environment, Bounds, useBounds, useGLTF, meshBounds, useHelper, Bvh} from "@react-three/drei"
+import { OrbitControls, Gltf, Environment, Bounds, useBounds, useGLTF, meshBounds, useHelper, Bvh, CameraControls} from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useCallback, useEffect, useRef, useState } from "react"
 import { BoxHelper, Group, Object3D, Vector3, BoxGeometry, MeshBasicMaterial } from "three"
+
 import { Box3 } from "three"
 type GlbModelProps = {
     name: string
 }
 
 const GlbModel = ({name}: GlbModelProps) => {
+    const [meshVisible, setMeshVisible] = useState<boolean>(false);
+    const [prevName, setPrevName] = useState(name);
+    if (prevName !== name) {
+        setPrevName(name);
+        setMeshVisible(false);
+    }
+
     const boundsApi = useBounds();
     const gltf = useGLTF(name);
-    gltf.scene.visible = false;
+    
     gltf.scene.updateMatrixWorld(true);
     const box = new Box3().setFromObject(gltf.scene);
     const groupRef = useRef<Group>(new Group());
     const meshRef = useRef<Object3D>(new Object3D());
 
     const currentScale = new Vector3(0,0,0);
-    const [meshVisible, setMeshVisible] = useState<boolean>(false);
     const [introAnimation, setIntroAnimation] = useState<boolean>(false);
+
 
     useEffect(() => {
         console.log("Refresh")
-        // boundsApi.refresh(box).fit().clip();
+        boundsApi.refresh(box).fit().clip();
 
-        
-        gltf.scene.visible = true;
-        //setMeshVisible(true);
-    }, [box, boundsApi]);
+    }, [box, boundsApi, gltf.scene, name]);
 
     useFrame(() => {
 
         if (introAnimation) {
+            console.log("Why")
             groupRef.current.scale.copy(currentScale);
-            currentScale.lerp(new Vector3(1,1,1), 0.1);
+            currentScale.lerp(new Vector3(1,1,1), 1);
+            
         }
+        setMeshVisible(true);
     });
 
     return (
-        <group ref={groupRef}>
+        <group visible={meshVisible} ref={groupRef}>
             <primitive onPointerEnter={()=>{console.log("Testing")}} ref={meshRef} object={gltf.scene}></primitive>
         </group>
     );
@@ -70,6 +78,7 @@ const ThreeViewer = () => {
     return (
         <Canvas>
             <OrbitControls></OrbitControls>
+            {/* <CameraControls makeDefault></CameraControls> */}
             <ambientLight  />
             <Environment preset="sunset" />
             <GalleryController></GalleryController>
